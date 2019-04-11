84218fb0c00ab47da7eeeb08c4e3d1b90605553c
lvedder <lamina-vedder@msn.com>
2019-04-11 16:07:21 +0200

    make author and year visible to customer

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,10 +1,21 @@
 package com.twu.biblioteca;
 
+import java.util.ArrayList;
+
 public class BibliotecaApp {
 
+    static ArrayList<Book> books = new ArrayList<Book>();
+
     public static void main(String[] args) {
+        setUpBooks();
         System.out.println(welcomeMessage());
         System.out.println(listOfBooks());
+    }
+
+    public static void setUpBooks() {
+        books.add(new Book("The Big Five For Life", "John Strelecky", "2008"));
+        books.add(new Book("The Why Are You Here Cafe", "John Strelecky", "2003"));
+//        books.add(new Book("The Alchemist", "Paulo Coelho", "1988"));
     }
 
     public static String welcomeMessage() {
@@ -13,6 +24,10 @@ public class BibliotecaApp {
     }
 
     public static String listOfBooks() {
-        return "The Big Five For Life\nDas Cafe am Rande der Welt\nThe Alchemist";
+        String bookList = "";
+        for(Book book : books) {
+            bookList = bookList + book.toString() + "\n";
+        }
+        return bookList;
     }
 }
