6483a2044ed1774932c7c2a4cae72afb53b1dfb8
lvedder <lamina-vedder@msn.com>
2019-04-11 12:11:13 +0200

    Added a welcome message

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -2,7 +2,19 @@ package com.twu.biblioteca;
 
 public class BibliotecaApp {
 
+    private static boolean hasPrintedWelcome = false;
+
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        if (!hasPrintedWelcome) {
+        System.out.println(welcomeMessage());
+        hasPrintedWelcome = true;
+    }
+        else {
+            System.out.println("Goodbye");
+        }
+    }
+
+    public static String welcomeMessage() {
+        return "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!";
     }
 }
